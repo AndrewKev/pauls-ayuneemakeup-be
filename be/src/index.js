d2e6fa4c42265e56a1b9919123949fe0840c2eeb
AndrewKev <akmkevin100@gmail.com>
2025-04-05 20:13:29 +0700

    feat : add cors

--- be/src/index.js
+++ be/src/index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const cookieParser = require("cookie-parser");
-
+const cors = require('cors');
 const app = express()
 const port = 8080
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
